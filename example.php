40ba594295c3f2469b1106a1f467c8413e304633
Tayfun Erbilen <tayfunerbilen@gmail.com>
2018-01-22 18:56:53 +0300

    Update example.php

--- example.php
+++ example.php
@@ -4,37 +4,38 @@ require_once __DIR__ . '/form.class.php';
 
 $form = new Prototurk\Form;
 
-$form->start('form.php', 'post', 'formClass');
-
-    $form->label('Kullanıcı Adınız', 'test');
+$form->start('index.php', 'post');
+    $form->label('Kullanıcı Adınız', 'username');
     $form->input('username', [
         'placeholder' => 'Kullanıcı Adı',
-        'class' => 'active',
-        'id' => 'test',
-        'required' => true
+        'required' => true,
+        'id' => 'username',
+        'class' => 'form-control'
     ]);
-    $form->label('Hakkımda');
-    $form->textarea('about_us', [
+    $form->label('Şifreniz', 'password');
+    $form->input('password', [
+        'placeholder' => 'Şifreniz',
+        'id' => 'password'
+    ]);
+    $form->label('Hakkında', 'about');
+    $form->textarea('about', [
+        'id' => 'about',
         'rows' => 5,
-        'cols' => 10
-    ], 'hello world!');
-    $form->label('Kategoriler');
-    $form->select('categories', [
-        '1' => 'Blog',
-        '2' => 'Web',
-        '3' => 'PHP'
+        'cols' => 5,
+        'placeholder' => 'Hakkınızda bir şeyler yazın'
+    ], 'tayfun erbilen');
+    $form->label('Cinsiyetiniz', 'gender');
+    $form->select('gender', [
+        1 => 'Kadın',
+        2 => 'Erkek'
     ], [
-        'multiple' => true,
-        'size' => 2,
-        'style' => 'width: 300px',
-        'id' => 'categories'
-    ], '2');
-    $form->button('Gönder');
-
+        'id' => 'gender',
+        'multiple' => true
+    ], 2);
+    $form->button('Giriş Yap', 'login_btn');
 $form->end();
 
-if (isset($_POST['submit']))
-{
+if (isset($_POST['submit'])){
     if ($form->control()){
         print_r($form->values());
     } else {
@@ -42,6 +43,6 @@ if (isset($_POST['submit']))
     }
 }
 
-echo $form->show('form');
+echo $form->show('login');
 
 //$form->dump();
